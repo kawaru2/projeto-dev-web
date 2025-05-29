8029b08553d4439cd1a692b622868f69c59dd59b
João Barros <joaopedrocb2@gmail.com>
2025-05-29 00:17:18 -0300

    ajustando o menu hamburguer para dispositivos moveis

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,32 +1,26 @@
 const menuBurguer = document.querySelector('.menu_burguer')
 
-// var action = true
-
 menuBurguer.addEventListener('click', (e) => {
 
   e.preventDefault()
 
-  // if (action == true) {
-  const menu = document.querySelector('.links_header')
+  var menu = document.querySelector('.links_header')
   const menuBurguer = document.querySelector('.menu_burguer')
-  const transition = document.querySelector('#lado_direito')
-
-  // menu.style.display = 'flex'
+  const transition = document.querySelector('.cabecalho')
+  var localResult = document.querySelector('.conteudo')
 
   if (menu.style.display == 'none' || menu.style.display == '') {
+
+    localResult.insertBefore(menu, localResult.firstChild)
     menu.style.display = 'flex'
-    transition.style.flexDirection = 'column'
+    menu.classList.toggle('new_menu')
+
   } else {
     menu.style.display = 'none'
-    transition.style.flexDirection = 'row'
+    menu.classList.toggle('new_menu')
+
   }
 
-  menu.classList.toggle('menu_mobile')
-
   menuBurguer.classList.toggle('menu_burguer_opened')
-
-  // action = false
-
-  // }
 
 })
