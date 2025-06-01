5d8a8e5734cdba615af3d2679ebb93a605061cab
João Barros <joaopedrocb2@gmail.com>
2025-06-01 18:12:22 -0300

    finalizando a função de criar o perfil e enviar o objeto para o localStorage.

--- scripts/criar_perfis.js
+++ scripts/criar_perfis.js
@@ -1,4 +1,5 @@
 export let profiles = []
+let dataBaseProfiles = JSON.parse(localStorage.getItem('Usuários'))
 
 // FUNÇÃO PARA ARMAZENAR EM UM ARRAY OS DADOS ENVIADOS PELO USUÁRIO.
 
@@ -14,60 +15,15 @@ function data() {
   }
 
   profiles.push(newProfile)
+  localStorage.setItem('Usuários', JSON.stringify(profiles))
+  alert(`Olá, ${newProfile.nome}. Seu perfil foi criado com sucesso! `)
 }
 
-// CRIAR UM CARTÃO COM INFORMAÇÕES ENVIADAS PELO USUÁRIO. TAIS INFORMAÇÕES VIRÃO DE UM ARRAY.
-
-// AQUI EU PRECISO PASSAR UM FOREACH() NO ARRAY PROFILES.
-
-// ESSA FUNÇÃO SERÁ USADA COMO BASE PARA O FOREACH()
-
-// function createProfile(profileData) {
-//   const listProfiles = document.querySelector('.perfis_pri')
-//   const profile = document.createElement('li')
-//   profile.classList.add('opcao_perf_pri')
-//   let id
-//   if (profileData.genero == 'masculino') {
-//     id = 'Ele/Dele'
-//   } else if (profileData.genero == 'feminino') {
-//     id = 'Ela/Dela'
-//   } else if (profileData.genero == 'outro') {
-//     id = '?/?'
-//   } else {
-//     console.log('genero não identificado')
-//   }
-//   profile.innerHTML = `
-//   <div class="cartao_inicio_pri">
-//       <img src="" class="foto_usuario">
-//       <p class="nome">${profileData.nome}</p> 
-//      </div>
-//      <div class="pronomes_e_idades">
-//       <p>${id}</p>
-//       <p>${profileData.idade}</p>
-//      </div>
-//      <div class="linha"></div>
-//      <p class="hobby">Hobby:<span class="resposta_hobby">${profileData.hobby}</span></p>
-//      <ul class="lista_dados_perfil">
-//       <li class="opcao_lista_dados_perfil">
-//        <p class="gostos_e_desgostos">Gosto de:</p>
-//        <p class="preferencias">Vôlei, Futebol, ciclismo, Séries, Filmes, Academia.</p>
-//       </li>
-//       <li class="opcao_lista_dados_perfil">
-//        <p class="gostos_e_desgostos">Não gosta de:</p>
-//        <p class="preferencias">ficar em casa, Festa, gente chata</p>
-//       </li>
-//      </ul>
-//      `
-//     //  listProfiles.appendChild(profile)
-//     console.log(profile)
-// }
-//
-//
-/////////////////////////////////////////////////////////////////////////////
+// FUNÇÃO PARA ADICIONAR OS CARTÕES NA TAG LISTA DO DOM.
 
 const listProfiles = document.querySelector('.perfis_pri')
 
-profiles.forEach((card, index) => {
+export let functionMain = profiles.forEach((card, index) => {
 
   let id
 
@@ -117,3 +73,49 @@ profiles.forEach((card, index) => {
      `
      listProfiles.appendChild(card)
 })
+
+// CRIAR UM CARTÃO COM INFORMAÇÕES ENVIADAS PELO USUÁRIO. TAIS INFORMAÇÕES VIRÃO DE UM ARRAY.
+
+// AQUI EU PRECISO PASSAR UM FOREACH() NO ARRAY PROFILES.
+
+// ESSA FUNÇÃO SERÁ USADA COMO BASE PARA O FOREACH()
+
+// function createProfile(profileData) {
+//   const listProfiles = document.querySelector('.perfis_pri')
+//   const profile = document.createElement('li')
+//   profile.classList.add('opcao_perf_pri')
+//   let id
+//   if (profileData.genero == 'masculino') {
+//     id = 'Ele/Dele'
+//   } else if (profileData.genero == 'feminino') {
+//     id = 'Ela/Dela'
+//   } else if (profileData.genero == 'outro') {
+//     id = '?/?'
+//   } else {
+//     console.log('genero não identificado')
+//   }
+//   profile.innerHTML = `
+//   <div class="cartao_inicio_pri">
+//       <img src="" class="foto_usuario">
+//       <p class="nome">${profileData.nome}</p> 
+//      </div>
+//      <div class="pronomes_e_idades">
+//       <p>${id}</p>
+//       <p>${profileData.idade}</p>
+//      </div>
+//      <div class="linha"></div>
+//      <p class="hobby">Hobby:<span class="resposta_hobby">${profileData.hobby}</span></p>
+//      <ul class="lista_dados_perfil">
+//       <li class="opcao_lista_dados_perfil">
+//        <p class="gostos_e_desgostos">Gosto de:</p>
+//        <p class="preferencias">Vôlei, Futebol, ciclismo, Séries, Filmes, Academia.</p>
+//       </li>
+//       <li class="opcao_lista_dados_perfil">
+//        <p class="gostos_e_desgostos">Não gosta de:</p>
+//        <p class="preferencias">ficar em casa, Festa, gente chata</p>
+//       </li>
+//      </ul>
+//      `
+//     //  listProfiles.appendChild(profile)
+//     console.log(profile)
+// }
